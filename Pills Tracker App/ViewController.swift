add1828f43ee8e7311193e95fdfa86f5c1300977
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-10-11 18:51:05 +0300

    add AnalyticsView

--- Pills Tracker App/ViewController.swift
+++ Pills Tracker App/ViewController.swift
@@ -4,15 +4,63 @@
 //
 //  Created by Nikita  on 10/10/22.
 //
-
+import SnapKit
 import UIKit
 
 class ViewController: UIViewController {
 
+    let greetingLabel = UILabel()
+    let dateLabel = UILabel()
+    let scrollView = UIScrollView()
+    let planProgressView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        initialize()
+        
     }
+    private func initialize(){
+        greetingLabel.textColor = UIColor(named: "Gray 1")
+        greetingLabel.textAlignment = .left
+        greetingLabel.font = UIFont.systemFont(ofSize: 16)
+        greetingLabel.text = "Have a great day!"
+        view.addSubview(greetingLabel)
+        
+        greetingLabel.snp.makeConstraints{ (make) in
+            make.top.equalTo(view).offset(68)
+            make.left.equalTo(view).offset(24)
+            make.right.equalTo(view).offset(-68)
+        }
+        
+        dateLabel.textColor = UIColor(named: "Dark")
+        dateLabel.textAlignment = .left
+        dateLabel.font = UIFont.boldSystemFont(ofSize: 34)
+        
+        dateLabel.text = "Thursday"
+        view.addSubview(dateLabel)
+        
+        dateLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(greetingLabel).offset(24)
+            make.left.equalTo(view).offset(24)
+            make.right.equalTo(view).offset(-206)
+        }
+        
+        view.addSubview(scrollView)
+        scrollView.isScrollEnabled = true
+        scrollView.contentSize = CGSize(width: 200, height: 750)
+        scrollView.snp.makeConstraints { make in
+            make.top.equalTo(dateLabel.snp.bottom).offset(10)
+            make.left.right.bottom.equalTo(view)
+        }
+        let analyticsView = setupAnalyticsView()
+        scrollView.addSubview(analyticsView)
+        analyticsView.frame = CGRect(x: 30, y: 10, width: 327, height: 126)
+        
+    }
+    
+    
+    
 
 
 }
---
+++ Pills Tracker App/ViewPieces/AnalyticsView.swift
@@ -0,0 +1,82 @@
+//
+//  AnalyticsView.swift
+//  Pills Tracker App
+//
+//  Created by Nikita  on 10/11/22.
+//
+
+import Foundation
+import UIKit
+
+ func setupAnalyticsView() -> UIView{
+    
+    let beginView = UIView()
+    beginView.layer.cornerRadius = 24
+    beginView.layer.borderColor = UIColor(named: "Gray 2")?.cgColor
+    beginView.layer.borderWidth = 1
+    
+    
+   
+    let labelView = UILabel()
+    beginView.addSubview(labelView)
+    labelView.numberOfLines = 2
+    labelView.textAlignment = .left
+    labelView.text = "Your plan\nis almost done!"
+    labelView.textColor = UIColor(named: "Dark")
+    labelView.font = UIFont.boldSystemFont(ofSize: 24)
+    labelView.frame.size.width = 177
+    labelView.frame.size.height = 56
+   
+    labelView.snp.makeConstraints { make in
+        make.top.equalTo(beginView.snp.top).offset(21)
+        make.left.equalTo(beginView.snp.left).offset(24)
+  
+    }
+    
+    let hStack = UIStackView()
+    hStack.axis = .horizontal
+    hStack.spacing = 4
+    let resultIcon = UIImageView()
+    resultIcon.contentMode = .scaleAspectFit
+    resultIcon.image = UIImage(named: "UpIcon")!
+    hStack.addArrangedSubview(resultIcon)
+    
+    let detailedTextLabel = UILabel()
+    detailedTextLabel.text = "13% than week ago"
+    detailedTextLabel.font = UIFont.systemFont(ofSize: 16)
+    detailedTextLabel.textColor = UIColor(named: "Gray 1")
+    hStack.addArrangedSubview(detailedTextLabel)
+    
+    beginView.addSubview(hStack)
+    hStack.snp.makeConstraints { make in
+        make.top.equalTo(labelView.snp.bottomMargin).offset(10)
+        make.left.equalTo(beginView.snp.left).offset(24)
+    }
+    
+    
+    let progressBar = CircularProgressView(frame: CGRect(x: 0, y: 0, width: 86, height: 86), lineWidth: 10, rounded: false)
+    progressBar.progressColor = UIColor(named: "Green")!
+    progressBar.trackColor = UIColor(named: "Green 2")!
+    progressBar.progress = 0.78
+    beginView.addSubview(progressBar)
+    progressBar.snp.makeConstraints { make in
+        make.right.equalTo(beginView).offset(-110)
+        make.top.equalTo(beginView.snp.top).offset(20)
+    }
+    
+    let progressLabel = UILabel()
+    progressLabel.textColor = UIColor(named: "Green")!
+    progressLabel.font = UIFont.boldSystemFont(ofSize: 20)
+    progressLabel.text = "78%"
+    progressBar.addSubview(progressLabel)
+    progressLabel.snp.makeConstraints { make in
+        make.top.equalTo(progressBar.snp.top).offset(30)
+        make.left.equalTo(progressBar.snp.left).offset(22)
+    }
+    
+  
+    return beginView
+    
+}
+
+
