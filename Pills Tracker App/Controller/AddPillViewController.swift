1d823fca67629292412babf69ce274c957948c30
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-10-20 22:23:53 +0300

    .

---
+++ Pills Tracker App/Controller/AddPillViewController.swift
@@ -0,0 +1,92 @@
+//
+//  AddPillViewController.swift
+//  Pills Tracker App
+//
+//  Created by Nikita  on 10/18/22.
+//
+
+import UIKit
+import SnapKit
+
+class AddPillViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let backButton = UIButton()
+//        backButton.setImage(UIImage(named: "backButton"), for: .normal)
+        backButton.frame = CGRect(x: 0, y: 0, width: 50, height: 20)
+       
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: backButton)
+
+        
+        let closeButton = UIButton()
+        closeButton.setImage(UIImage(named: "closeButton2"), for: .normal)
+        closeButton.frame = CGRect(x: 0, y: 0, width: 50, height: 20)
+        closeButton.addTarget(self, action: #selector(closeVC), for: .touchUpInside)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: closeButton)
+        
+        
+        initialise()
+
+        
+    }
+    
+    private func initialise(){
+        let pageNumber = UILabel()
+        pageNumber.text = "1 is 2"
+        pageNumber.textColor = UIColor(named: "Gray 1")
+        pageNumber.font = UIFont.systemFont(ofSize: 16)
+        view.addSubview(pageNumber)
+        pageNumber.snp.makeConstraints { make in
+            make.left.equalTo(view.snp.left).offset(24)
+            make.top.equalTo(view.snp.top).offset(100)
+        }
+        
+        let titleLabel = UILabel()
+        titleLabel.text = "Add medication"
+        titleLabel.textColor = UIColor(named: "Dark")
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 34)
+        view.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(pageNumber.snp.bottom).offset(12)
+            make.left.equalTo(pageNumber.snp.left)
+        }
+        
+        let buttonsView = UIView()
+        view.addSubview(buttonsView)
+        buttonsView.frame = CGRect(x: 20, y: 215, width: 338, height: 64)
+      
+        let button1 = ChooseMedicationImageButton()
+        buttonsView.addSubview(button1)
+        button1.frame = CGRect(x: 10, y: 0, width: 64, height: 64)
+        let button2 = ChooseMedicationImageButton()
+        buttonsView.addSubview(button2)
+        button2.frame = CGRect(x: 98, y: 0, width: 64, height: 64)
+        let button3 = ChooseMedicationImageButton()
+        buttonsView.addSubview(button3)
+        button3.frame = CGRect(x: 186, y: 0, width: 64, height: 64)
+        let button4 = ChooseMedicationImageButton()
+        buttonsView.addSubview(button4)
+        button4.frame = CGRect(x: 274, y: 0, width: 64, height: 64)
+        
+        deselectAllButtons()
+        
+        
+    }
+    
+    
+    
+    func deselectAllButtons(){
+        
+    }
+  
+    
+    @objc func closeVC(){
+        navigationController?.popViewController(animated: true)
+    }
+    
+
+     
+
+}
---
+++ Pills Tracker App/Controller/SecondAddPillViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondAddPillViewController.swift
+//  Pills Tracker App
+//
+//  Created by Nikita  on 10/18/22.
+//
+
+import UIKit
+
+class SecondAddPillViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
