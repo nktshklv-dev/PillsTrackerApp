85cf7f32538c3fbf47075298fea7cbf8d0e24044
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-11-23 10:29:27 +0300

    Date type extension

--- Pills Tracker App/Controller/ViewController.swift
+++ Pills Tracker App/Controller/ViewController.swift
@@ -174,4 +174,14 @@ extension ViewController{
 }
 
 
+extension Date {
+    func dayOfWeek() -> String? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        return dateFormatter.string(from: self).capitalized
+        // or use capitalized(with: locale) if you want
+    }
+}
+
+
 
